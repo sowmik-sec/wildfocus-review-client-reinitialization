3762059ef8fe5ea545989acc9ce9c24fd6c09afa
sowmik-sec <sowmik.sec@gmail.com>
2023-03-23 16:44:22 +0600

    photo click to full screen added

--- src/pages/Home/FeaturedServices/Service.js
+++ src/pages/Home/FeaturedServices/Service.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Image.css";
 
 const Service = ({ service }) => {
   const { instructorName, serviceImg, title, instructorImage, description } =
     service;
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  const handleClick = () => {
+    setIsFullScreen(!isFullScreen);
+  };
 
   return (
     <div className="card w-96 bg-base-100 shadow-xl mx-auto">
       <figure>
-        <img src={serviceImg} alt="photography" className="h-60 w-full" />
+        <img
+          src={serviceImg}
+          alt="photography"
+          className="image"
+          onClick={handleClick}
+        />
+        {isFullScreen && (
+          <div className="fullscreen-overlay" onClick={handleClick}>
+            <img className="fullscreen-image" src={serviceImg} alt="" />
+          </div>
+        )}
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
