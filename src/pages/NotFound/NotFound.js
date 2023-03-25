d6699eccf0b88f5ee6b3ad2af769fbf60e82b74d
sowmik-sec <sowmik.sec@gmail.com>
2023-03-25 12:24:03 +0600

    not found page updated

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
+    <div className="min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 flex flex-col justify-center items-center">
       <div className="max-w-2xl mx-auto text-center">
-        <h1 className="text-6xl font-bold text-gray-900">404</h1>
-        <p className="text-2xl font-medium mb-8">Oops! Page not found</p>
+        <h1 className="text-6xl font-extrabold text-white mb-6">404</h1>
+        <p className="text-2xl font-medium text-white mb-8">
+          Oops! Page not found
+        </p>
         <button
-          className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
           onClick={() => window.history.back()}
         >
           Go Back
