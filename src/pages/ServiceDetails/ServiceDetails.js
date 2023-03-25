1c4ca0c5571e12a0243589bb19dbc42c21c37926
sowmik-sec <sowmik.sec@gmail.com>
2023-03-25 09:39:37 +0600

    service details style changed

--- src/pages/ServiceDetails/ServiceDetails.js
+++ src/pages/ServiceDetails/ServiceDetails.js
@@ -1,10 +1,28 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import Navbar from "../../Shared/Navbar/Navbar";
 
 const ServiceDetails = () => {
-  const service = useLoaderData();
-  console.log(service);
-  return <div>this is details</div>;
+  const { title, serviceImg, description, instructorName, instructorImage } =
+    useLoaderData();
+  return (
+    <div>
+      <Navbar />
+      <div className="w-1/2 mx-auto">
+        <img src={serviceImg} alt="" className=" h-auto" />
+        <h3 className="text-3xl">{title}</h3>
+        <div className="flex items-center">
+          <img
+            src={instructorImage}
+            alt=""
+            className="w-14 h-14 rounded-full"
+          />
+          <p className="ml-2">{instructorName}</p>
+        </div>
+        <p className="">{description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceDetails;
