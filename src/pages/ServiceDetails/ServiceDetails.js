de83d2d56b5e4e6c40898b2c7a410d00917519da
sowmik-sec <sowmik.sec@gmail.com>
2023-03-25 17:20:13 +0600

    sending review to server

--- src/pages/ServiceDetails/ServiceDetails.js
+++ src/pages/ServiceDetails/ServiceDetails.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import Review from "../../Review/Review";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
 import Navbar from "../../Shared/Navbar/Navbar";
 
 const ServiceDetails = () => {
-  const { title, serviceImg, description, instructorName, instructorImage } =
-    useLoaderData();
+  const {
+    _id,
+    title,
+    serviceImg,
+    description,
+    instructorName,
+    instructorImage,
+  } = useLoaderData();
+  const { user } = useContext(AuthContext);
+  const [review, setReview] = useState("");
+
+  const { displayName, photoURL, email } = user;
+  const data = {
+    review,
+    displayName,
+    photoURL,
+    email,
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(review);
+    sendReviewToServer();
+    setReview("");
+  };
+
+  const sendReviewToServer = () => {
+    fetch(`http://localhost:5000/review/${_id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleInputChange = (event) => {
+    setReview(event.target.value);
+  };
   return (
     <div>
       <Navbar />
@@ -22,7 +63,28 @@ const ServiceDetails = () => {
         </div>
         <p className="">{description}</p>
       </div>
-      <Review />
+      <div className="w-1/2 mx-auto border border-slate-300">
+        {user ? (
+          <>
+            <input
+              type="text"
+              value={review}
+              onChange={handleInputChange}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSubmit(e);
+                }
+              }}
+              className="py-2 px-3 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400"
+              placeholder="Add Review..."
+            />
+          </>
+        ) : (
+          <>
+            <h3 className="text-3xl">Please login to add a review</h3>
+          </>
+        )}
+      </div>
     </div>
   );
 };
