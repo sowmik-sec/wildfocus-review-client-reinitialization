ac89ebfc0cfd4d086015038813704c45319f0d09
sowmik-sec <sowmik.sec@gmail.com>
2023-03-25 11:53:19 +0600

    Set an authentication state observer and get user data

--- src/AuthProvider/AuthProvider.js
+++ src/AuthProvider/AuthProvider.js
@@ -1,8 +1,9 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.config";
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
 } from "firebase/auth";
 
@@ -10,13 +11,20 @@ const auth = getAuth(app);
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState("");
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const login = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
-  const authInfo = { createUser, login };
+  useEffect(() => {
+    const unSubscriber = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unSubscriber;
+  }, []);
+  const authInfo = { createUser, login, user };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
