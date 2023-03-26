852edb19f5694585b9efc2e975444418cbb12871
sowmik-sec <sowmik.sec@gmail.com>
2023-03-26 13:04:03 +0600

    fetching current users review from server side

--- src/pages/MyReviews/MyReviews.js
+++ src/pages/MyReviews/MyReviews.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
 import Navbar from "../../Shared/Navbar/Navbar";
 
 const MyReviews = () => {
+  const { user } = useContext(AuthContext);
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/my-review?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setReviews(data))
+      .catch((err) => console.error(err));
+  }, [user.email]);
+  console.log(reviews);
   return (
     <div>
       <Navbar />
