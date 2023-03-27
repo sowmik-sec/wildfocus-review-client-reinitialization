04c007b5acbec2aac95287b2eed4fd2b732a0d6c
sowmik-sec <sowmik.sec@gmail.com>
2023-03-27 11:11:26 +0600

    creating link to update review page

--- src/pages/MyReviews/MyReviews.js
+++ src/pages/MyReviews/MyReviews.js
@@ -1,6 +1,7 @@
 import { faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import Navbar from "../../Shared/Navbar/Navbar";
 import "./MyReviews.css";
@@ -8,6 +9,8 @@ import "./MyReviews.css";
 const MyReviews = () => {
   const { user } = useContext(AuthContext);
   const [reviews, setReviews] = useState([]);
+  const [singleReview, setSingleReview] = useState({});
+  const [editReview, setEditReview] = useState(singleReview.review);
   useEffect(() => {
     fetch(`http://localhost:5000/my-review?email=${user.email}`)
       .then((res) => res.json())
@@ -31,6 +34,48 @@ const MyReviews = () => {
         .catch((err) => console.error(err));
     }
   };
+
+  // var updateId = -1;
+  // var update = {};
+
+  // const handleEdit = (id) => {
+  //   updateId = id;
+  //   console.log("inside function id = ", id);
+  //   console.log("inside function updateId = ", updateId);
+
+  //   fetch(`http://localhost:5000/review-single/${id}`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setSingleReview(data);
+  //       setEditReview(data.review);
+  //       update = {
+  //         review: data.review,
+  //         displayName: data.displayName,
+  //         email: data.email,
+  //         photoURL: data.photoURL,
+  //         serviceId: data.serviceId,
+  //         _id: data._id,
+  //       };
+  //       console.log(update);
+  //     });
+  // };
+  // const handleUpdateReview = (id) => {
+  //   fetch(`http://localhost:5000/review-update/${id}`, {
+  //     method: "PATCH",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(update),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       if (data.modifiedCount > 0) {
+  //         alert("Review Updated");
+  //       }
+  //     });
+  // };
+
   return (
     <div>
       <Navbar />
@@ -59,7 +104,9 @@ const MyReviews = () => {
                       <td className="wrap-text">{review}</td>
                       <td>{timestamp}</td>
                       <td>
-                        <FontAwesomeIcon icon={faPen} title="Edit Review" />
+                        <Link to={`/update-review/${_id}`}>
+                          <FontAwesomeIcon icon={faPen} title="Edit Review" />
+                        </Link>
                         <FontAwesomeIcon
                           icon={faTrashCan}
                           className="ml-3"
